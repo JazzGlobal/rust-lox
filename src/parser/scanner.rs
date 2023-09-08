6df07155becc204872ea7182bf8b02d3e95701c0
Chris Gambrell <cgambrell@orchardsoft.com>
2023-09-08 16:15:53 -0400

    Support string literals and numbers.

--- src/parser/scanner.rs
+++ src/parser/scanner.rs
@@ -1,5 +1,5 @@
-use std::fmt::format;
 use crate::parser::token::{Token, TokenType};
+use std::fmt::format;
 
 #[derive(Debug)]
 pub struct Scanner {
@@ -8,6 +8,7 @@ pub struct Scanner {
     current: usize,
     start: usize,
     line: i32,
+    current_col: i32,
 }
 
 impl Scanner {
@@ -15,6 +16,7 @@ impl Scanner {
         self.start = 0;
         self.current = 0;
         self.line = 1;
+        self.current_col = 0;
 
         while !self.is_at_end() {
             self.start = self.current;
@@ -27,106 +29,164 @@ impl Scanner {
             lexeme: "".to_string(),
             literal: "".to_string(),
             line: self.line,
+            col: self.current_col,
         });
     }
 
     fn scan_token(&mut self) {
         let current_char: char = self.advance();
         match current_char {
-            '(' => {self.add_token(TokenType::LEFT_PAREN, current_char.to_string())}
-            ')' => {self.add_token(TokenType::RIGHT_PAREN, current_char.to_string())}
-            '{' => {self.add_token(TokenType::LEFT_BRACE, current_char.to_string())}
-            '}' => {self.add_token(TokenType::RIGHT_BRACE, current_char.to_string())}
-            ',' => {self.add_token(TokenType::COMMA, current_char.to_string())}
-            '.' => {self.add_token(TokenType::DOT, current_char.to_string())}
-            '-' => {self.add_token(TokenType::MINUS, current_char.to_string())}
-            '+' => {self.add_token(TokenType::PLUS, current_char.to_string())}
-            ';' => {self.add_token(TokenType::SEMICOLON, current_char.to_string())}
-            '*' => {self.add_token(TokenType::STAR, current_char.to_string())}
-            '!' => {
-                match self.match_value('=') {
-                    true => { self.add_token(TokenType::BANG_EQUAL, "!=".to_string()) },
-                    false => { self.add_token(TokenType::BANG, "!".to_string()) },
-                }
-            }
-            '=' => {
-                match self.match_value('=') {
-                    true => { self.add_token(TokenType::EQUAL_EQUAL, "==".to_string()) },
-                    false => { self.add_token(TokenType::EQUAL, "=".to_string()) }
-                }
-            }
-            '<' => {
-                match self.match_value('=') {
-                    true => { self.add_token(TokenType::LESS_EQUAL, "<=".to_string()) }
-                    false => { self.add_token(TokenType::LESS, "<".to_string()) }
-                }
-            }
-            '>' => {
-                match self.match_value('=') {
-                    true => { self.add_token(TokenType::GREATER_EQUAL, ">=".to_string()) }
-                    false => { self.add_token(TokenType::GREATER, ">".to_string()) }
-                }
-            }
-            '/' => {
-                match self.match_value('/') {
-                    true => {
-                        while self.peek() != '\n' && !self.is_at_end() { self.advance(); }
+            '(' => self.add_token(TokenType::LEFT_PAREN, current_char.to_string()),
+            ')' => self.add_token(TokenType::RIGHT_PAREN, current_char.to_string()),
+            '{' => self.add_token(TokenType::LEFT_BRACE, current_char.to_string()),
+            '}' => self.add_token(TokenType::RIGHT_BRACE, current_char.to_string()),
+            ',' => self.add_token(TokenType::COMMA, current_char.to_string()),
+            '.' => self.add_token(TokenType::DOT, current_char.to_string()),
+            '-' => self.add_token(TokenType::MINUS, current_char.to_string()),
+            '+' => self.add_token(TokenType::PLUS, current_char.to_string()),
+            ';' => self.add_token(TokenType::SEMICOLON, current_char.to_string()),
+            '*' => self.add_token(TokenType::STAR, current_char.to_string()),
+            '!' => match self.match_value('=') {
+                true => self.add_token(TokenType::BANG_EQUAL, "!=".to_string()),
+                false => self.add_token(TokenType::BANG, "!".to_string()),
+            },
+            '=' => match self.match_value('=') {
+                true => self.add_token(TokenType::EQUAL_EQUAL, "==".to_string()),
+                false => self.add_token(TokenType::EQUAL, "=".to_string()),
+            },
+            '<' => match self.match_value('=') {
+                true => self.add_token(TokenType::LESS_EQUAL, "<=".to_string()),
+                false => self.add_token(TokenType::LESS, "<".to_string()),
+            },
+            '>' => match self.match_value('=') {
+                true => self.add_token(TokenType::GREATER_EQUAL, ">=".to_string()),
+                false => self.add_token(TokenType::GREATER, ">".to_string()),
+            },
+            '/' => match self.match_value('/') {
+                true => {
+                    while self.peek(None) != '\n' && !self.is_at_end() {
+                        self.advance();
                     }
-                    false => { self.add_token(TokenType::SLASH, "/".to_string()) }
                 }
-            }
+                false => self.add_token(TokenType::SLASH, "/".to_string()),
+            },
             '\n' => {
                 self.line += 1;
+                self.current_col = 0;
             }
-            ' ' | '\r' | '\t' => {
-                return
+            ' ' | '\r' | '\t' => return,
+
+            '\"' => {
+                self.handle_string_literal();
             }
-            default =>
+
+            default => {
+                if default.is_numeric()
                 {
-                    let x = format!("Unexpected character {0} on Line {1}", default, self.line);
+                    self.handle_number();
+                } else {
+                    let x = format!(
+                        "Unexpected character {0} on Line {1}, col {2}",
+                        default, self.line, self.current_col
+                    );
                     println!("{}", x);
                 }
+            }
         };
     }
 
     fn add_token(&mut self, token_type: TokenType, literal: String) {
-        dbg!(&self);
-
         let text = self.source[self.start..self.current].to_string();
         self.tokens.push(Token {
             token_type,
             lexeme: text,
             literal,
             line: self.line,
+            col: self.current_col,
         });
     }
 
     fn is_at_end(&self) -> bool {
-        return &self.current >= &self.source.len()
+        return &self.current >= &self.source.len();
     }
 
     fn advance(&mut self) -> char {
         let result = self.source.chars().nth(self.current).unwrap_or('|');
         self.current += 1;
+        self.current_col += 1;
         result
     }
 
     fn match_value(&mut self, expected: char) -> bool {
-        // dbg!("Inside match_value: {0}", &expected);
-        if self.is_at_end() || self.source.chars().nth(self.current).unwrap_or('|') != expected
-        {
-            return false
+        if self.is_at_end() || self.source.chars().nth(self.current).unwrap_or('|') != expected {
+            return false;
         }
         self.current += 1;
+        self.current_col += 1;
         true
     }
 
-    fn peek(&self) -> char {
-        if self.is_at_end() { return '\0' }
-        return self.source.chars().nth(self.current).unwrap_or('|');
+    fn peek(&self, by_num: Option<i32>) -> char {
+        if self.is_at_end() {
+            return '\0';
+        }
+
+        return match by_num {
+            None => { self.source.chars().nth(self.current).unwrap_or('|') }
+            Some(num) =>
+                {
+                    if self.current + (num as usize) >= self.source.len() {
+                        return '\0';
+                    }
+                    self.source.chars().nth(self.current + (num as usize)).unwrap_or('|')
+                }
+        }
+    }
+
+    fn handle_string_literal(&mut self) {
+        while self.peek(None) != '\"' && !self.is_at_end() {
+            if self.peek(None) == '\n' {
+                self.line += 1;
+                self.current_col = 0;
+            }
+            self.advance();
+        }
+
+        if self.is_at_end() {
+            println!(
+                "Unterminated string. Line {0} Col {1}",
+                self.line, self.current_col
+            );
+            return;
+        }
+
+        self.advance();
+        let value = self.source[self.start + 1..self.current - 1].to_string();
+        self.add_token(TokenType::STRING, value);
+    }
+
+    fn handle_number(&mut self) {
+        while self.peek(None).is_numeric()
+        {
+            self.advance();
+        }
+        if self.peek(None) == '.' && self.peek(Some(1)).is_numeric() {
+            self.advance();
+            while self.peek(None).is_numeric() {
+                self.advance();
+            }
+        }
+        self.add_token(TokenType::NUMBER, self.source[self.start..self.current].to_string())
     }
 }
 
 pub fn create_scanner(source: String) -> Scanner {
-    Scanner {source, tokens: vec![], current: 0 , start: 0, line: 1}
+    Scanner {
+        source,
+        tokens: vec![],
+        current: 0,
+        start: 0,
+        line: 1,
+        current_col: 0,
+    }
 }
--- src/parser/token.rs
+++ src/parser/token.rs
@@ -4,82 +4,196 @@ use std::fmt::{Display, Formatter};
 #[derive(Debug)]
 pub enum TokenType {
     // Single-character tokens.
-    LEFT_PAREN, RIGHT_PAREN, LEFT_BRACE, RIGHT_BRACE,
-    COMMA, DOT, MINUS, PLUS, SEMICOLON, SLASH, STAR,
+    LEFT_PAREN,
+    RIGHT_PAREN,
+    LEFT_BRACE,
+    RIGHT_BRACE,
+    COMMA,
+    DOT,
+    MINUS,
+    PLUS,
+    SEMICOLON,
+    SLASH,
+    STAR,
 
     // One or two character tokens.
-    BANG, BANG_EQUAL,
-    EQUAL, EQUAL_EQUAL,
-    GREATER, GREATER_EQUAL,
-    LESS, LESS_EQUAL,
+    BANG,
+    BANG_EQUAL,
+    EQUAL,
+    EQUAL_EQUAL,
+    GREATER,
+    GREATER_EQUAL,
+    LESS,
+    LESS_EQUAL,
 
     // Literals.
-    IDENTIFIER, STRING, NUMBER,
+    IDENTIFIER,
+    STRING,
+    NUMBER,
 
     // Keywords.
-    AND, CLASS, ELSE, FALSE, FUN, FOR, IF, NIL, OR,
-    PRINT, RETURN, SUPER, THIS, TRUE, VAR, WHILE,
+    AND,
+    CLASS,
+    ELSE,
+    FALSE,
+    FUN,
+    FOR,
+    IF,
+    NIL,
+    OR,
+    PRINT,
+    RETURN,
+    SUPER,
+    THIS,
+    TRUE,
+    VAR,
+    WHILE,
 
-    EOF
+    EOF,
 }
 
 impl Display for TokenType {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-       match &self {
-           TokenType::LEFT_PAREN => {write!(f, "LEFT_PAREN")}
-           TokenType::RIGHT_PAREN => {write!(f, "RIGHT_PAREN")}
-           TokenType::LEFT_BRACE => {write!(f, "LEFT_BRACE")}
-           TokenType::RIGHT_BRACE => {write!(f, "RIGHT_PAREN")}
-           TokenType::COMMA => {write!(f, "COMMA")}
-           TokenType::DOT => {write!(f, "DOT")}
-           TokenType::MINUS => {write!(f, "MINUS")}
-           TokenType::PLUS => {write!(f, "PLUS")}
-           TokenType::SEMICOLON => {write!(f, "SEMICOLON")}
-           TokenType::SLASH => {write!(f, "SLASH")}
-           TokenType::STAR => {write!(f, "STAR")}
-           TokenType::BANG => {write!(f, "BANG")}
-           TokenType::BANG_EQUAL => {write!(f, "BANG_EQUAL")}
-           TokenType::EQUAL => {write!(f, "EQUAL")}
-           TokenType::EQUAL_EQUAL => {write!(f, "EQUAL_EQUAL")}
-           TokenType::GREATER => {write!(f, "GREATER")}
-           TokenType::GREATER_EQUAL => {write!(f, "GREATER_EQUAL")}
-           TokenType::LESS => {write!(f, "LESS")}
-           TokenType::LESS_EQUAL => {write!(f, "LESS_EQUAL")}
-           TokenType::IDENTIFIER => {write!(f, "IDENTIFIER")}
-           TokenType::STRING => {write!(f, "STRING")}
-           TokenType::NUMBER => {write!(f, "NUMBER")}
-           TokenType::AND => {write!(f, "AND")}
-           TokenType::CLASS => {write!(f, "CLASS")}
-           TokenType::ELSE => {write!(f, "ELSE")}
-           TokenType::FALSE => {write!(f, "FALSE")}
-           TokenType::FUN => {write!(f, "FUN")}
-           TokenType::FOR => {write!(f, "FOR")}
-           TokenType::IF => {write!(f, "IF")}
-           TokenType::NIL => {write!(f, "NIL")}
-           TokenType::OR => {write!(f, "OR")}
-           TokenType::PRINT => {write!(f, "PRINT")}
-           TokenType::RETURN => {write!(f, "RETURN")}
-           TokenType::SUPER => {write!(f, "SUPER")}
-           TokenType::THIS => {write!(f, "THIS")}
-           TokenType::TRUE => {write!(f, "TRUE")}
-           TokenType::VAR => {write!(f, "VAR")}
-           TokenType::WHILE => {write!(f, "WHILE")}
-           TokenType::EOF => {write!(f, "EOF")}
-       }
+        match &self {
+            TokenType::LEFT_PAREN => {
+                write!(f, "LEFT_PAREN")
+            }
+            TokenType::RIGHT_PAREN => {
+                write!(f, "RIGHT_PAREN")
+            }
+            TokenType::LEFT_BRACE => {
+                write!(f, "LEFT_BRACE")
+            }
+            TokenType::RIGHT_BRACE => {
+                write!(f, "RIGHT_PAREN")
+            }
+            TokenType::COMMA => {
+                write!(f, "COMMA")
+            }
+            TokenType::DOT => {
+                write!(f, "DOT")
+            }
+            TokenType::MINUS => {
+                write!(f, "MINUS")
+            }
+            TokenType::PLUS => {
+                write!(f, "PLUS")
+            }
+            TokenType::SEMICOLON => {
+                write!(f, "SEMICOLON")
+            }
+            TokenType::SLASH => {
+                write!(f, "SLASH")
+            }
+            TokenType::STAR => {
+                write!(f, "STAR")
+            }
+            TokenType::BANG => {
+                write!(f, "BANG")
+            }
+            TokenType::BANG_EQUAL => {
+                write!(f, "BANG_EQUAL")
+            }
+            TokenType::EQUAL => {
+                write!(f, "EQUAL")
+            }
+            TokenType::EQUAL_EQUAL => {
+                write!(f, "EQUAL_EQUAL")
+            }
+            TokenType::GREATER => {
+                write!(f, "GREATER")
+            }
+            TokenType::GREATER_EQUAL => {
+                write!(f, "GREATER_EQUAL")
+            }
+            TokenType::LESS => {
+                write!(f, "LESS")
+            }
+            TokenType::LESS_EQUAL => {
+                write!(f, "LESS_EQUAL")
+            }
+            TokenType::IDENTIFIER => {
+                write!(f, "IDENTIFIER")
+            }
+            TokenType::STRING => {
+                write!(f, "STRING")
+            }
+            TokenType::NUMBER => {
+                write!(f, "NUMBER")
+            }
+            TokenType::AND => {
+                write!(f, "AND")
+            }
+            TokenType::CLASS => {
+                write!(f, "CLASS")
+            }
+            TokenType::ELSE => {
+                write!(f, "ELSE")
+            }
+            TokenType::FALSE => {
+                write!(f, "FALSE")
+            }
+            TokenType::FUN => {
+                write!(f, "FUN")
+            }
+            TokenType::FOR => {
+                write!(f, "FOR")
+            }
+            TokenType::IF => {
+                write!(f, "IF")
+            }
+            TokenType::NIL => {
+                write!(f, "NIL")
+            }
+            TokenType::OR => {
+                write!(f, "OR")
+            }
+            TokenType::PRINT => {
+                write!(f, "PRINT")
+            }
+            TokenType::RETURN => {
+                write!(f, "RETURN")
+            }
+            TokenType::SUPER => {
+                write!(f, "SUPER")
+            }
+            TokenType::THIS => {
+                write!(f, "THIS")
+            }
+            TokenType::TRUE => {
+                write!(f, "TRUE")
+            }
+            TokenType::VAR => {
+                write!(f, "VAR")
+            }
+            TokenType::WHILE => {
+                write!(f, "WHILE")
+            }
+            TokenType::EOF => {
+                write!(f, "EOF")
+            }
+        }
     }
 }
 
 #[derive(Debug)]
-pub struct Token
-{
+pub struct Token {
     pub token_type: TokenType,
     pub lexeme: String,
     pub literal: String,
     pub line: i32,
+    pub col: i32,
 }
 
 impl Display for Token {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{}", format!("{0} {1} {2}", &self.token_type, &self.lexeme, &self.literal))
+        write!(
+            f,
+            "{}",
+            format!(
+                "{0} {1} {2} {3}",
+                &self.token_type, &self.lexeme, &self.literal, &self.col
+            )
+        )
     }
 }
